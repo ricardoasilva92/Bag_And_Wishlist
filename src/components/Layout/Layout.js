6656063e66855837a28215b909ad9fa97a5253e8
Ricardo Silva <ricardoaraujo.silva@farfetch.com>
2019-08-19 18:00:00 +0100

    Crs-3.4: Create wishlist component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React, { useState, useContext } from "react";
 import { BasketContext } from "../../BasketContext";
 import Styles from "./Layout.module.css";
 import Bag from "../Bag/Bag";
+import Wishlist from "../Wishlist/Wishlist"
 
 const Layout = () => {
   
   return (
     <div className={Styles.layout}>
       <Bag />
+      <Wishlist />
     </div>
   );
 };
---
+++ src/components/Wishlist/Wishlist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from './Wishlist.module.css'
+
+const Wishlist = () => (
+    <section className={Styles.wishlist}>
+        <h1>Wishlist</h1>
+    </section>
+)
+
+export default Wishlist;

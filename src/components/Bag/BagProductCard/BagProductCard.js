bb935d4f0bf47702da781a09eb7fde269c9a3d29
Ricardo Silva <ricardoasilva1992@gmail.com>
2019-08-28 23:12:40 +0100

    Crs-3.11: Move product from Bag to Wishlist

--- src/components/Bag/BagProductCard/BagProductCard.js
+++ src/components/Bag/BagProductCard/BagProductCard.js
@@ -1,44 +1,45 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Styles from "./BagProductCard.module.css";
 import { BasketContext } from "../../../BasketContext";
 
 const BagProductCard = props => {
     const [basket, setBasket] = useContext(BasketContext);
 
-	const handleClick = () => {
+    const handleClick = () => {
         setBasket(prevBasket => {
             const bagProducts = prevBasket.Bag.products;
-            bagProducts.splice(props.index,1);
+            let productToMove = bagProducts.splice(props.index, 1)[0];
+            prevBasket.Wishlist.products.push(productToMove);
             prevBasket.Bag.products = bagProducts;
-            
-            return { ...prevBasket};
-        });
-	};
 
-	return (
-		<div className={Styles.container}>
-			<div className={Styles.imageContainer}>
-				<img
-					className={Styles.image}
-					src={props.product.image}
-					alt="Gucci X Mickey Mouse tote bag"
-				/>
-			</div>
-			<div className={Styles.productInfo}>
-				<ul>
-					<li>Brand: {props.product.brand}</li>
-					<li>Name: {props.product.name}</li>
-					<li>Price: {props.product.price}</li>
-					<li>Quantity: {props.product.quantity}</li>
-				</ul>
-			</div>
-			<div className={Styles.addToWishlist}>
-				<button type="button" onClick={handleClick}>
-					Add to Wishlist!
-				</button>
-			</div>
-		</div>
-	);
+            return { ...prevBasket };
+        });
+    };
+
+    return (
+        <div className={Styles.container}>
+            <div className={Styles.imageContainer}>
+                <img
+                    className={Styles.image}
+                    src={props.product.image}
+                    alt="Gucci X Mickey Mouse tote bag"
+                />
+            </div>
+            <div className={Styles.productInfo}>
+                <ul>
+                    <li>Brand: {props.product.brand}</li>
+                    <li>Name: {props.product.name}</li>
+                    <li>Price: {props.product.price}</li>
+                    <li>Quantity: {props.product.quantity}</li>
+                </ul>
+            </div>
+            <div className={Styles.addToWishlist}>
+                <button type="button" onClick={handleClick}>
+                    Add to Wishlist!
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default BagProductCard;
